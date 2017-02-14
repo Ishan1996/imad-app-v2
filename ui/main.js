d6440115b5e01c3b77a869b57ae66dae988c9a9e
Ishan1996 <ishanchoudhury1996@gmail.com>
2017-02-14 14:41:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,12 +20,24 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['Ishan','Raj','Ayush'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request= new XMLHttpRequest();
+    request.onreadystatechange= function(){
+        if(request.readystate=== XMLHttprequest.DONE){
+            if(request.status===200){
+                    var names=request.responseText;
+                    names=JSON.parse(names);
+                    var list='';
+                    for(var i=0;i<names.length;i++)
+                    {
+                        list += '<li>' + names[i] + '</li>';
+                    }
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML = list;
+            }
+        }
+    };
+    var nameInput=document.getElementById(name);
+    var name=nameInput.value;
+    request.open('GET', 'http://http://ishan1996.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
